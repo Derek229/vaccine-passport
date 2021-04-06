28aea4baeae4ed0db2116692298cc7a192a144ae
Derek cluff <cluffderek@yahoo.com>
2021-04-06 17:01:14 -0600

    models and seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+# user seed
+15.times do
+  User.create(
+first_name: Faker::Name.first_name,
+last_name: Faker::Name.last_name,
+age: Faker::Number.number(digits: 2),
+gender: Faker::Gender.short_binary_type,
+email: Faker::Internet.email,
+role: 'user',
+country_origin: Faker::Address.country,
+image: '',
+password:'123456'
+)
+end
+
+#this is verifier seed
+
+verifier1=User.create(first_name:'beth', last_name:'johnson', email: 'verifier1@test.com', role:'verifier', password:'123456')
+verifier2=User.create(first_name:'james', last_name:'bond', email: 'verifier2@test.com', role:'verifier', password:'123456')
+verifier3=User.create(first_name:'tedd', last_name:'lang', email: 'verifier3@test.com', role:'verifier', password:'123456')
+verifier4=User.create(first_name:'john', last_name:'doe', email: 'verifier4@test.com', role:'verifier', password:'123456')
+verifier5=User.create(first_name:'jane', last_name:'doe', email: 'verifier5@test.com', role:'verifier', password:'123456')
+
+
+
+
+#country seed 
+5.times do
+  User.create(
+    country_origin: Faker::Address.country,
+    role:'issuer'
+  )
+end
+
+puts 'completed'
