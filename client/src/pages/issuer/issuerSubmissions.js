a93541579e65934103a24bc50cacad1d261169c0
Trevor von Hake <tvonhake@outlook.com>
2021-04-06 18:16:29 -0600

    created folders and components for issuer/verifier

---
+++ client/src/pages/issuer/issuerSubmissions.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//TODO: determine what issuer UI should be (check trello board)
+
+const issuerSubmissions = () => {
+  return (
+    <div>
+      <h1>render submissions for issuer</h1>
+    </div>
+  )
+}
+
+export default issuerSubmissions
