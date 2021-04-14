e40da244e400f86bdceed14f6503c2fc089164f9
Adam Mazur <adammazue@gmail.com>
2021-04-14 14:06:18 -0600

    FilePond Basics

--- client/src/pages/user/UploadVaccImage.js
+++ client/src/pages/user/UploadVaccImage.js
@@ -1,46 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import * as FilePond from 'filepond'
-import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
-import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import React, { useState } from 'react'
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+import "filepond/dist/filepond.min.css";
+import { FilePond, File, registerPlugin } from "react-filepond";
 import { Button, Image } from "react-bootstrap";
 import axios from 'axios'
 
-const UploadVaccImage = () => {
-  const [files, setFiles] = useState([])
+function BasicUpload() {
+  const [files, setFiles] = useState([]);
+  const handleUpdate = async (fileItems) => {
+    try {
+      setFiles(fileItems);
 
-const handleClick = async ()=>{
-  console.log(files)
-//   files[0].file; file is the thing I want to send to backend
-try {
-   let data = new FormData()
-    // files[0].file  this will come in as params file
-   data.append('file', files[0].file)
-//    data.append('x', 'x here')
-   let res  = await axios.post('/api/users/${user.id}/vaccination_wallets', data)
-   console.log(res.data)
-} catch(err) {
-  console.log(err)
-  console.log(err.response.data)
-  alert('error occurred')
+      // appending 'file' with image info to pass can retieve in params
+      let data = new FormData();
+      data.append("file", fileItems[0].file);
+      const res = await Axios.post(`/api/basicUpload?name=test`, data);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err.response); //err.response from axios
+      alert("error in upload");
+    }
+  };
+  return (
+    <div>
+      <FilePond
+        files={files}
+        onupdatefiles={handleUpdate}
+        // onupdatefiles={setFiles}
+        allowMultiple={false}
+        // maxFiles={3}
+        // server="/api/basicUpload"
+        name="files"
+        // {/* sets the file input name, it's filepond by default */}
+        labelIdle='Drag  Drop your files or <span class="filepond--label-action">Browse</span>'
+      />
+    </div>
+  );
 }
-}
 
-return (
-  <div className="App">
-    <FilePond
-      files={files}
-      allowReorder={true}
-      // allowMultiple={true}
-      allowMultiple={true}
-      onupdatefiles={setFiles}
-      labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
-    />
-
-    <Button onClick={handleClick}>add</Button>
-    {uzers.length >= 1 && <Image src={uzers[0].image} />}
-  </div>
-);
 
 
 const UploadVaccImage = () => {
@@ -49,8 +46,6 @@ const UploadVaccImage = () => {
 
     </div>
   )
-}
-
 }
 
 export default UploadVaccImage
