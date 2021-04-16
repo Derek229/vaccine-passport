bb37911db84c8d9cd545b95dacd19a95462ff9fb
Trevor von Hake <tvonhake@outlook.com>
2021-04-15 20:11:05 -0600

    images show in user dashboard

--- client/src/pages/user/UserVaccine.js
+++ client/src/pages/user/UserVaccine.js
@@ -39,8 +39,9 @@ const UserVaccine = (props) => {
 
   return(
     <div key={vaccine_id}>
-      <Card >
+      <Card style={{ width: '25rem' }} className="mr-1">
         <Card.Body>
+        <Card.Img variant="top" src={vaccination.image}/>
           <Card.Title><h3>Vaccination: {vaccine_name}</h3></Card.Title>
           <Card.Text>
             MFG: {manufacturer}
