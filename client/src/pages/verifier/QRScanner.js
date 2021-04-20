eca1a3cdd443315aa4e1c25201fe4b45c279f6ac
Derek cluff <cluffderek@yahoo.com>
2021-04-20 17:28:49 -0600

    qr code scanner added

--- client/src/pages/verifier/QRScanner.js
+++ client/src/pages/verifier/QRScanner.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {AuthContext} from '../../providers/AuthProvider'
+import QReader from '../../components/QrReader'
 
 const QRScanner = () => {
+
+  const auth = useContext(AuthContext)
+
   return (
     <div>
-      <h1>QR Reader Here</h1>
+      <QReader auth={auth}/>
     </div>
   )
 }
