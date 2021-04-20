656c1a138d4cb8f01976342aa8a6216b159dc627
ahmazur19 <79327886+ahmazur19@users.noreply.github.com>
2021-04-20 11:42:57 -0600

    Public page (#27)
    * merge from master
    
    * Console Warning Cleanup
    
    * AllVaccines show page working routes worked
    
    * NavBar Routing issue
    
    * Login Routes adjusted for role

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -45,8 +45,12 @@ class Navbar1 extends React.Component {
         </>
       )
     }
-  }
   
+  }
+  // if (user.role === 'user') {
+  //   return(
+  //     <Nav.Link href= > </Nav.Link>
+  // }
   render() {
     return (
       <div>
@@ -58,7 +62,7 @@ class Navbar1 extends React.Component {
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/about">About</Nav.Link>
               <Nav.Link href="/users/self/wallet">Wallet</Nav.Link>
-              <Nav.Link href="/users/self/wallet">Wallet</Nav.Link>
+							<Nav.Link href="/users/allVaccines">Vaccines</Nav.Link>
               <Nav.Link href="/users/verifier/VerifierHomePage">Verifier Home</Nav.Link>
               <NavDropdown title="Menu">
                 <NavDropdown.Item href="/users/issuer/vaccines">Issuers Vaccines</NavDropdown.Item>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,28 @@
 import React, {useEffect, useState} from 'react'
 import Login from '../components/Login'
 import {Link} from 'react-router-dom'
+import UserDashboard from '../pages/user/UserDashboard';
+import IssuerVaccines from '../pages/issuer/IssuerVaccines';
+import VerifierHomePage from '../pages/verifier/VerifierHomePage'
 
-const Home = () => {
+const Home = (props) => {
 
+	const user = props
+
+if (user.role === 'user'){
+return(
+    <UserDashboard />
+)}
+else if (user.role === 'issuer'){
+  return(
+    <IssuerVaccines/>
+  )}
+  else if (user.role === 'verifier'){
+    return(
+      <VerifierHomePage/>
+    )}
+
+  else {
   return (
     <div>
         <Login/>
@@ -14,7 +33,7 @@ const Home = () => {
   <Link to='/verifierLogin'>Country origin access</Link>
   </div>
     </div>
-  )
-}
+  )}
+  }
 
 export default Home
