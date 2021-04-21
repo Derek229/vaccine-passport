f37af17f76cd618713e3831812cf982fa1a102c3
Trevor von Hake <tvonhake@outlook.com>
2021-04-20 18:25:25 -0600

    add title to page

--- client/src/pages/verifier/QRScanner.js
+++ client/src/pages/verifier/QRScanner.js
@@ -14,7 +14,9 @@ const QRScanner = () => {
   }
 
   return (
-    <div>
+    <div >
+      {showScanner === false && <h1 style={{marginTop: '20px'}}>Scan a User's QR Code Here</h1>}
+      {showScanner === false && <h4 style={{marginTop: '20px'}}>click the button below to get started</h4>}
       <Button style={{margin: '20px'}} onClick={show}>{showScanner === false ? 'Open QR Scanner' : 'Close QR Scanner'}</Button>
       {showScanner === true && <QReader auth={auth}/>}
     </div>
