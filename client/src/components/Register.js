1b59f74fd4b6de2a2d8bbf2d13369a9f0ee0f23f
Trevor von Hake <tvonhake@outlook.com>
2021-04-21 22:01:59 -0600

    adam & derek's changes to registration

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,20 +1,20 @@
 import React, { useContext, useEffect, useState} from 'react'
-import {Form, Button} from 'react-bootstrap'
+import {Form, Button, Row, Col} from 'react-bootstrap'
 import { AuthContext } from '../providers/AuthProvider'
-
 const Register = ( {history} ) => {
   const { handleRegister, authErrors, setAuthErrors } = useContext(AuthContext);
-  const [email, setEmail] = useState(null) 
+  const [email, setEmail] = useState(null)
+	const [role, setRole] = useState('')
   const [password, setPassword] = useState(null)
   const [passwordConfirmation, setPasswordConfirmation] = useState(null)
   useEffect(() =>{
     setAuthErrors([]);
   },[]);
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     if(password === passwordConfirmation){
       handleRegister({
+				role,
         email,
         password,
         passwordConfirmation,
@@ -24,11 +24,33 @@ const Register = ( {history} ) => {
     }else alert("Passwords Don't Match");
   };
 
+  
   return (
     <>
     <h1>Register New Account</h1>
     {authErrors && authErrors.map((err) => <p>{err}</p>)}
     <Form onSubmit={handleSubmit}>
+    <Form.Group as={Row}>
+      <Form.Label as="legend" column sm={2}>
+				Are you a User or Verifier?
+      </Form.Label>
+      <Col sm={10}>
+        <Form.Check
+          type="radio"
+          label="user"
+          name="role"
+          id="formHorizontalRadios1"
+					onChange={(e) => setRole(e.target.labels[0].innerText)}
+        />
+        <Form.Check
+          type="radio"
+          label="verifier"
+          name="role"
+          id="formHorizontalRadios2"
+					onChange={(e) => setRole(e.target.labels[0].innerText)}
+        />
+      </Col>
+    </Form.Group>
       <Form.Group>
         <Form.Control
           label="Email"
@@ -65,7 +87,6 @@ const Register = ( {history} ) => {
         <Button variant="primary" type='submit'>Submit</Button>
     </Form>
     </>
-
 );
 };
 export default Register;
