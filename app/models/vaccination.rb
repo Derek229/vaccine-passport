e066fc8074525940a576829ac1f2c9b4046a51d4
Trevor von Hake <tvonhake@outlook.com>
2021-04-21 21:07:00 -0600

    admin functionalities

--- app/models/vaccination.rb
+++ app/models/vaccination.rb
@@ -3,10 +3,10 @@ class Vaccination < ApplicationRecord
   belongs_to :vaccine
 
   def self.all_vaccinations
-    select('vx.id, u.id as user_id, u.name as user_name, v.name as vaccine_name, vx.image as vaccination_image, vx.issuer_name')
-    .from('vaccinations vx')
-    .joins("inner join users u on u.id = vx.user_id
-     inner join vaccines v on v.id = vx.vaccine_id")
+    select('vaccinations.id, vaccinations.user_id as vaccination_user_id, users.id as user_id, first_name, last_name, vaccines.id as vaccine_id, vaccines.name as vaccine_name, vaccines.date as date, manufacturer, vaccinations.image, vaccinations.issuer_name')
+    .from('vaccinations')
+    .joins('inner join users on vaccinations.user_id = users.id 
+    inner join vaccines on vaccinations.vaccine_id = vaccines.id')
 
   end
 
