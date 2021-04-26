468b8e9d3449c6cb584a1101608d29e065b43963
Derek cluff <cluffderek@yahoo.com>
2021-04-26 14:21:04 -0600

    verifier styling

--- client/src/pages/verifier/QRScanner.js
+++ client/src/pages/verifier/QRScanner.js
@@ -1,7 +1,7 @@
 import React, {useContext, useState} from 'react'
 import {AuthContext} from '../../providers/AuthProvider'
 import QReader from '../../components/QrReader'
-import {Button, Container} from 'react-bootstrap'
+import {Button, Card, Container} from 'react-bootstrap'
 
 const QRScanner = () => {
 
@@ -15,12 +15,12 @@ const QRScanner = () => {
 
   return (
     <div style={{margin:'0 auto',  width: '80%' }}>
-      <Container style={{display:'flex', flexDirection:'column', alignItem:'center', justifyContent:'center' }}>
+      <Card style={{display:'flex', flexDirection:'column', alignItem:'center', justifyContent:'center', paddingTop:'15px', marginTop: '25px' }}>
       {showScanner === false && <h1 style={{marginTop: '20px'}}>Scan a User's QR Code Here</h1>}
       {showScanner === false && <h4 style={{marginTop: '20px'}}>click the button below to get started</h4>}
       <Button style={{margin: '20px'}} onClick={show}>{showScanner === false ? 'Open QR Scanner' : 'Close QR Scanner'}</Button>
       {showScanner === true && <QReader style={{display:'flex', flexDirection:'column', alignItem:'center', justifyContent:'center' }} auth={auth}/>}
-      </Container>
+      </Card>
     </div>
   )
 }
