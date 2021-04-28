d368677ec424fc2dc9fa959def12d8d3473edaf7
tvonhake <60283897+tvonhake@users.noreply.github.com>
2021-04-28 14:58:40 -0600

    Style touchups (#81)
    * smaller image files.
    
    * update rails
    
    * fix get verifiers
    
    * replace adam placeholder image
    
    * fix image and linkedin link in about for adam.
    
    * register form accepts names.
    Favicon and site title updated
    
    * Protected Routes also protected by role
    
    * Admin can create a user
    
    * cleanup console warnings.
    
    * fix login page image
    
    * update image styling for santiago and derek about
    
    * more about styling
    
    * fix github icon margins
    
    * shorten carousel top margin
    
    * issuer issue and CRUD pages finished.

---
+++ client/src/pages/issuer/IssuerVaccination.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import axios from 'axios'
+import {Button} from 'react-bootstrap'
+
+
+const IssuerVaccination = (props) => {
+
+  const {vaccination, setVaccinations, vaccinations, auth} = props
+
+  const deleteVaccination = async () => {
+    try{
+      let res = await axios.delete(`/api/users/${auth.user.id}/vaccinations/${vaccination.id}`)
+      const filteredVaccinations = vaccinations.filter(x=>x.id !== vaccination.id)
+      setVaccinations(filteredVaccinations)
+      console.log(res.data)
+    }catch(err){
+      alert(err)
+      console.log(err)
+    }
+  }
+
+  return(
+    <>
+      <tr>
+        <td>{vaccination.id}</td>
+        <td>{vaccination.vaccine_name}</td>
+        <td>{vaccination.manufacturer}</td>
+        <td>{vaccination.email}</td>
+        <td>
+          <Button className="ml-1 btn btn-danger" onClick={()=>deleteVaccination()}>Delete</Button>
+        </td>
+      </tr>
+    </>
+      )
+}
+
+export default IssuerVaccination
