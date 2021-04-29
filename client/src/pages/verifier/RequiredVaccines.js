dd46c04260185ee2cc0247adeaf4fceca3c512ec
Derek229 <79328028+Derek229@users.noreply.github.com>
2021-04-29 11:24:13 -0600

    Veifier styling cleanup (#94)
    * verifier backend
    
    * removed debugger
    
    * nav changes
    
    * styling
    
    * cleanuped verifeir styling
    
    * stuff
    
    * verifier styling last bit of cleanup

--- client/src/pages/verifier/RequiredVaccines.js
+++ client/src/pages/verifier/RequiredVaccines.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 import {useEffect} from 'react'
-import { Container, Button, Form } from 'react-bootstrap';
+import { Container, Button, Form, Table } from 'react-bootstrap';
 import axios from 'axios'
 import { AuthContext } from '../../providers/AuthProvider';
 import { Typeahead } from 'react-bootstrap-typeahead'
@@ -59,10 +59,10 @@ const RequiredVaccine=()=> {
     const requiredVaccineForm = () =>{
       return(
         <>
-        <Form onSubmit= {handleSubmit}>
+        <Form onSubmit= {handleSubmit} style={{marginBottom:'15px', backgroundColor:'white'}}>
         <Form.Group>
-          <Form.Label>Add a Vaccine to Required List</Form.Label>
-          <Typeahead
+          <Form.Label><h3 style={{marginLeft:'10px', marginTop:"10px"}}>Add a Vaccine to Required List</h3></Form.Label>
+          <Typeahead style={{marginLeft:'10px', marginRight:'15px'}}
             id="vaccine"
             labelKey="name"
             onChange={setVaccineSelection}
@@ -71,27 +71,40 @@ const RequiredVaccine=()=> {
             options={vaccineOptions}
           />
         </Form.Group>
-        <Button type='submit'> verify </Button>
+        <Button style={{marginLeft:'10px', marginBottom:'10px', width:'80px'}}type='submit'> add </Button>
         </Form>
         </>
         )
       }
     
-      
+      const reqTable = () => {
+        return(
+        <Table style={{padding: '0px 0px 0px 50px ', margin: '0', backgroundColor: 'white'}} responsive striped hover>
+          <thead>
+            <tr>
+              <th><p style={{marginLeft:'100px',marginTop:'20px'}}>Vaccine Name</p></th>
+              <th><p style={{marginBottom:'15px'}}>Action</p></th>
+            </tr>
+          </thead>
+          <tbody>
+            {renderReqVaccine()}
+          </tbody>
+        </Table>
+        )
+      }
 
 
   return (
     <div>
       <InPageNavNoCenter auth={auth}/>
-      <Container >
+      <Container border='primary'>
       <div className="tablecontainer" style={{backgroundColor: 'white', marginBottom: '15px', marginTop: '15px', padding: '10px'}}>
         <h1>Required Vaccines List</h1>
         <Button onClick={() => setShowForm(!showForm)}>{showForm ? 'Hide Form' : 'Add New Required Vaccine'}</Button>
         </div>
         {showForm && requiredVaccineForm()}
-        {renderReqVaccine()}
-        
-      </Container>
+        {reqTable()}
+        </Container>  
     </div>
 
   )
